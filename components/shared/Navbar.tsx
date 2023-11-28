8fa47d241f05762313f4166ee93230c246b4601f
arielmerinos <arielmerino@ciencias.unam.mx>
2023-11-28 00:17:54 -0600

    Fix bug in login functionality

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,17 @@
+
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12'>
+        <Link href='/' className='flez items-center gap-1'>
+            <Image src='/assets/images/site-logo.svg' width={23} height={23} alt='OverflowDev Logo' />
+            <p className='h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden'>Dev <span className='text-primary-500'>Flow</span></p>
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar
