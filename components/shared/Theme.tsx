0735f095b2bc2dee12d58d1bf58a2288dd884e44
arielmerinos <arielmerino@ciencias.unam.mx>
2023-11-29 20:42:23 -0600

    Add @radix-ui/react-menubar dependency

---
+++ components/shared/Theme.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useTheme } from '@/context/ThemeProvider'
+const Theme = () => {
+
+    const {mode, setMode} = useTheme();
+    
+  return (
+    <div>Theme</div>
+  )
+}
+
+export default Theme
