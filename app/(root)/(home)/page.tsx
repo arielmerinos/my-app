03292acff593ab17a49b27b7a62a7b73089cfb1f
arielmerinos <arielmerino@ciencias.unam.mx>
2023-11-29 20:20:52 -0600

    Remove UserButton and add GlobalSearch, Theme, and MobileNavBar to Navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,7 +5,6 @@ import { join } from 'path'
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl='/'/>
       <h1 className='h1-bold'>Next 14 We're coming!</h1>
       <h2 className='h2-bold'>Next 14 We're coming!</h2>
       <h3 className='h3-bold'>Next 14 We're coming!</h3>
