7116eeb10b699ba78bb3ce5a0feff6f480b370c5
arielmerinos <arielmerino@ciencias.unam.mx>
2023-12-01 12:29:42 -0600

    Add themes and sidebar links to constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,43 @@
+import { SidebarLink } from "@/types";
 export const themes = [
     {value: 'light', label: 'Light', icon: '/assets/icons/sun.svg'},
     {value: 'dark', label: 'Dark', icon: '/assets/icons/moon.svg'},
     {value: 'system', label: 'System', icon: '/assets/icons/computer.svg'},
-
 ]
+
+export const sidebarLinks: SidebarLink[] = [
+    {
+        imgURL: '/assets/icons/home.svg',
+        label: 'Home',
+        route: '/',
+    },
+    {
+        imgURL: '/assets/icons/users.svg',
+        route: '/comminity',
+        label: 'Community',
+    },
+    {}
+]
+
+export const BADGE_CRITERIA = {
+    QUESTION_COUNT: {
+        BRONZE: 10,
+        SILVER: 50,
+        GOLD: 100,
+    },
+    ANSWER_COUNT: {
+        BRONZE: 10,
+        SILVER: 50,
+        GOLD: 100,
+    },
+    QUESTION_UPVOTES: {
+        BRONZE: 10,
+        SILVER: 50,
+        GOLD: 100,
+    },
+    ANSWER_UPVOTES: {
+        BRONZE: 10,
+        SILVER: 50,
+        GOLD: 100,
+    },
+}
