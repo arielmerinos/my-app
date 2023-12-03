41f79099f429ae01db78b52483b004709221e759
arielmerinos <arielmerino@ciencias.unam.mx>
2023-12-03 13:33:21 -0600

    Refactor home page styling

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <div className='text-dark400_light700' >
       <h1 className='h1-bold'>Next 14 We're coming!</h1>
       <h2 className='h2-bold'>Next 14 We're coming!</h2>
       <h3 className='h3-bold'>Next 14 We're coming!</h3>
