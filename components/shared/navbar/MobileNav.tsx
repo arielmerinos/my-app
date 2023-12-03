69e22884abfc7cfa878b394d2eaa10062c1e119d
arielmerinos <arielmerino@ciencias.unam.mx>
2023-12-03 11:59:32 -0600

    Add mobile navigation functionality and update dev logo Update dev logo and add sign-in and sign-up buttons Update dev script to run on specific IP address

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import NavContent from './NavContent'
 
 import {
     Sheet,
+    SheetClose,
     SheetContent,
     SheetDescription,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
+import { SignedOut } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
 
 const MobileNav = () => {
   return (
@@ -25,8 +29,33 @@ const MobileNav = () => {
                 height={23}
                 alt='OverflowDev Logo'
                 />
-                <p className='h2-bold text-dark-100_light900 font-spaceGrotesk dark:text-light-900 max-sm:hidden'> Dev <span className='text-primary-500'>Overflow</span></p>
+                <p className='h2-bold text-dark-100_light900 font-spaceGrotesk dark:text-light-900 '> Dev <span className='text-primary-500'>Overflow</span></p>
             </Link >
+            <div>
+              <SheetClose asChild>
+                <NavContent /> 
+              </SheetClose>
+
+              <SignedOut>
+                <div className='flex flex-col gap-3'>
+                  <SheetClose asChild>
+                    <Link href='/sign-in'>
+                      <Button className=' w-full small-medium btn-secondary min-h-[41px] rounded-lg px-4 py-3 shadow-none'>
+                        <span className='primary-text-gradient'>Log In </span>
+                      </Button>
+                    </Link>  
+                  </SheetClose>
+
+                  <SheetClose asChild>
+                    <Link href='/sign-'>
+                      <Button className='w-full small-medium light-border-2 btn-tertiary min-h-[41px] text-dark400_light900 rounded-lg px-4 py-3 shadow-none'>
+                        <span className='text-dark400_light900'>Sign Up </span>
+                      </Button>
+                    </Link>  
+                  </SheetClose>
+                </div>
+              </SignedOut>
+            </div>
         </SheetContent>
     </Sheet>
   )
---
+++ components/shared/navbar/NavContent.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { SheetClose } from '@/components/ui/sheet'
+import { sidebarLinks } from '@/constants'
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+
+
+const NavContent = () => {
+    const pathName = usePathname();
+    let isActive = false;
+
+  return (
+    <section className='flex h-full flex-col gap-6 pt-16'>
+        {sidebarLinks.map((item) => {
+
+            isActive = (pathName.includes(item.route) && item.route.length > 1) || pathName === item.route;
+
+            return (
+            <SheetClose asChild key={item.route}>
+                <Link 
+                href={item.route}
+                className={`${isActive ? 'primary-gradient rounded-lg text-light-900': 'text-dark300_light900 '} flex items-center justify-start gap-4 bg-transparent p-4`}
+                >
+                    <Image
+                    src={item.imgURL}
+                    alt={item.label}
+                    width={20}
+                    height={20}
+                    className={`${isActive ? '':'invert-colors'}`}
+                    />
+                    <p className={`${isActive ? 'base-bold': ' base-medium'}`}>{item.label}</p>
+                </Link>
+            </SheetClose>)
+        })}
+
+    </section>
+  )
+}
+
+export default NavContent
